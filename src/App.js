9460d7a0149f56c7e68f6c4676afa0755e6614bd
kobii <harold.rockata@gmail.com>
2023-07-08 18:02:28 +0200

    Finished first two steps for phonebook app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,55 @@
+import { useState } from 'react'
+
 const App = (props) => {
+  const [persons, setPersons] = useState(props.persons)
+  const [newName, setNewName] = useState("")
+
+  const addName = (event) => {
+    event.preventDefault()
+
+    if (
+      persons
+      .filter((person) => person.name === newName)
+      .length > 0
+    ) {
+      console.log(`${newName} already in list`)
+      return (false)
+    }
+
+    setPersons(
+      persons.concat(
+        {
+          id: persons.length + 1,
+          name: newName,
+        }
+      )
+    )
+    setNewName("")
+    return (true)
+  }
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
   return (
     <div>
-      Hello, World!
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map(person => <li key={person.id}>{person.name}</li>)}
+      </ul>
     </div>
   )
 }
 
-export default App;
+export default App
