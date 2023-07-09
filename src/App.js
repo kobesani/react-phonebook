9b3ccab07045cd691ce7a982833b7ca1c64fcd16
kobii <harold.rockata@gmail.com>
2023-07-09 18:25:50 +0200

    Support for adding numbers and filtering entries added.. person state is now object

--- src/App.js
+++ src/App.js
@@ -2,17 +2,44 @@ import { useState } from 'react'
 
 const App = (props) => {
   const [persons, setPersons] = useState(props.persons)
-  const [newName, setNewName] = useState("")
+  const [filteredPersons, setFilteredPersons] = useState(props.persons)
+  const [newEntry, setNewEntry] = useState({name: "", number: ""})
+  const [searchTerm, setSearchTerm] = useState("")
 
-  const addName = (event) => {
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewEntry(
+      existingValues => (
+        {
+          ...existingValues,
+          name: event.target.value
+        }
+      )
+    )
+  }
+
+  const handleNumberChange = (event) => {
+    console.log(event.target.value)
+    setNewEntry(
+      existingValues => (
+        {
+          ...existingValues,
+          number: event.target.value
+        }
+      )
+    )
+  }
+
+  const addEntry = (event) => {
     event.preventDefault()
 
     if (
       persons
-      .filter((person) => person.name === newName)
+      .filter((person) => person.name === newEntry.name)
       .length > 0
     ) {
-      console.log(`${newName} already in list`)
+      console.log(`${newEntry.name} already in list`)
+      window.alert(`${newEntry.name} is already in the phonebook!`)
       return (false)
     }
 
@@ -20,25 +47,43 @@ const App = (props) => {
       persons.concat(
         {
           id: persons.length + 1,
-          name: newName,
+          name: newEntry.name,
+          number: newEntry.number,
         }
       )
     )
-    setNewName("")
+    setNewEntry({name: "", number: ""})
     return (true)
   }
 
-  const handleNameChange = (event) => {
-    console.log(event.target.value)
-    setNewName(event.target.value)
+  const handleSearchTermChange = (event) => {
+    console.log(event.target.value.toLowerCase())
+    setSearchTerm(event.target.value)
+
+    const filteredPersons = persons.filter(
+      (person) =>
+        person.name.toLowerCase().includes(event.target.value.toLowerCase())
+    )
+    console.log(filteredPersons)
+    // filtered persons is a subset of the phonebook entries
+    setFilteredPersons(
+      persons.filter(
+        (person) =>
+          person.name.toLowerCase().includes(event.target.value.toLowerCase())
+      )
+    )
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addName}>
+      <h2>Filter</h2>
+      <input value={searchTerm} onChange={handleSearchTermChange}/>
+      <h2>Add a new entry</h2>
+      <form onSubmit={addEntry}>
         <div>
-          name: <input value={newName} onChange={handleNameChange} />
+          name: <input value={newEntry.name} onChange={handleNameChange} /><br/>
+          number: <input value={newEntry.number} onChange={handleNumberChange} />
         </div>
         <div>
           <button type="submit">add</button>
@@ -46,7 +91,14 @@ const App = (props) => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map(person => <li key={person.id}>{person.name}</li>)}
+        {
+          filteredPersons.map(
+            person =>
+              <li key={person.id}>
+                {person.name} {person.number}
+              </li>
+          )
+        }
       </ul>
     </div>
   )
